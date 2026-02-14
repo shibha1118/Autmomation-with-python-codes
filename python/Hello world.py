3f4ef9e628c3d77f3e12a4eba1511d3881be73d2
shibha1118 <shivabhargav1118@gmail.com>
2026-02-14 16:06:10 +0530

    Create Hello world.py

---
+++ python/Hello world.py
@@ -0,0 +1,11 @@
+#This program will say hello and ask questions
+print("hello world!")
+print("whats your name")
+My_name = input(">")
+print("your name is :", My_name)
+print("How old are you", My_name)
+My_age = input(">")
+print("Wow you are :", My_age)
+print("You will be about", str(int(My_age) + 1), "next year")
+
+
