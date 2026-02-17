165e8c54d1dc9589f4f618d94c93dd5a2f8107bb
Shiva Bhargav <shivabhargav1118@gmail.com>
2026-02-17 22:44:14 +0530

    Create basic password check.py

---
+++ python codes/chapter 2/basic password check.py
@@ -0,0 +1,12 @@
+# password check
+user = 'shiva'
+password = 'testpassword101'
+print('Please enter user name and password in next line')
+user_name = input('>')
+password_ = input('>')
+if ((user == user_name) and (password == password_)):
+    print('Welcome '+ user)
+else:
+    print('Please check your details')
+
+
